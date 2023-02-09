1c296527633361d555fd3baed4b1345b6a161061
Thanveer <122423969+Thanveer1202@users.noreply.github.com>
2023-02-09 23:29:33 +0530

    Add files via upload

---
+++ reverse_number using function.cpro.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int reverse(int);
+int main(){
+    int num, rev_num, rem;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    rev_num = reverse(num);
+    printf("Reversed number is: %d", rev_num);  
+    return 0;
+}
+
+int reverse(int n)
+{
+    int rem, rev = 0;
+    while (n != 0)
+	{
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
+    }
+    return rev;
+}
